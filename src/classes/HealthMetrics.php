6ad1c04d3af80675d77970e71dd54dcf6dd67592
pomit101 <ittipoldk@gmail.com>
2026-04-07 22:45:58 +0700

    Create HealthMetrics.php file with class for calculating Life Expectancy, HALE, and YLL

---
+++ src/classes/HealthMetrics.php
@@ -0,0 +1,28 @@
+<?php
+
+class HealthMetrics {
+    // Method to calculate Life Expectancy
+    public function calculateLifeExpectancy($data) {
+        // Implementation here
+    }
+
+    // Method to calculate Healthy Life Expectancy (HALE)
+    public function calculateHALE($data) {
+        // Implementation here
+    }
+
+    // Method to calculate Years of Life Lost (YLL)
+    public function calculateYLL($data) {
+        // Implementation here
+    }
+
+    // Method for data comparison
+    public function compareData($data1, $data2) {
+        // Implementation here
+    }
+
+    // Method for trend analysis
+    public function analyzeTrend($data) {
+        // Implementation here
+    }
+}
